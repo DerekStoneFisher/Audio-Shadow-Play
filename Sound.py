9636c50b6e54ff16c280b88c3d44f7c76625293c
Derek Fisher <defisher@ciis.canon.com>
2018-08-22 17:29:33 -0700

    - beginning of refactoring - moving sound playing functionality out of the main loop and into SoundCollection and SoundEntry - starting to flesh out SoundBoardGUI

--- Sound.py
+++ Sound.py
@@ -3,6 +3,7 @@ import pyaudio
 import json
 from Audio_Proj_Const import KEY_ID_TO_NAME_MAP, convertJavaKeyIDToRegularKeyID
 import os
+import time, thread
 
 
 
@@ -52,6 +53,31 @@ class SoundCollection:
         for soundEntry in soundEntries:
             self.addSoundEntry(soundEntry)
 
+    def playSound(self, keys_down_tuple, last_keys_down_tuple, hold_to_play=Fal, restart_instead_of_stop=True):
+        if frozenset(keys_down_tuple) in self.key_bind_map: # if the bind for a sound was pressed
+            #temp_previous_sound_entry = sound_entry
+            sound_entry = self.key_bind_map[frozenset(keys_down_tuple)]
+            #if temp_previous_sound_entry.path_to_sound != sound_entry.path_to_sound:
+            #    previous_sound_entry = temp_previous_sound_entry
+
+
+            if not sound_entry.is_playing: # start playing it if it not playing
+                thread.start_new_thread(sound_entry.play, tuple())
+            elif not hold_to_play: # stop playing it if hold_to_play is off and the key was let go
+                thread.start_new_thread(sound_entry.stop, tuple())
+                if restart_instead_of_stop: # only start playing the sound over again if restart_after_stopping is true
+                    counter = 0
+                    while sound_entry.stream_in_use and counter < 1000: # wait for the sound_entry to finish outputting its current chunk to the stream if it is in the middle of doing so
+                        time.sleep(.001)
+                        counter += 1
+                    thread.start_new_thread(sound_entry.play, tuple())
+                else:
+                    restart_instead_of_stop = True # toggling restart_after_stopping off only lasts for one sound-key-press
+        elif hold_to_play and frozenset(last_keys_down_tuple) in self.key_bind_map: # if hold to play is on and we just let go of the key for a sound
+            sound_entry = self.key_bind_map[frozenset(last_keys_down_tuple)]
+            thread.start_new_thread(sound_entry.stop, tuple())
+
+
 
 class SoundEntry:
     def __init__(self, path_to_sound, frames=None, activation_keys=[], is_playing=False, continue_playing=True, pitch_modifier=0):
@@ -92,6 +118,20 @@ class SoundEntry:
             output_device_index=5
         )
 
+
+    def playMultiThreaded(self):
+        print "playing", self.path_to_sound
+        if not self.is_playing: # start playing it if it not playing
+            thread.start_new_thread(self.play, tuple())
+        else: # stop playing it if hold_to_play is off and the key was let go
+            thread.start_new_thread(self.stop, tuple())
+            counter = 0
+            while self.stream_in_use and counter < 1000: # wait for the self to finish outputting its current chunk to the stream if it is in the middle of doing so
+                time.sleep(.001)
+                counter += 1
+            thread.start_new_thread(self.play, tuple())
+
+
     def play(self, reset_frame_index=True):
         self.is_playing = True
         self.continue_playing = True
@@ -113,7 +153,7 @@ class SoundEntry:
             current_frame = self.frames[frame_index]
             current_frame = Audio_Utils.getPitchShiftedFrame(current_frame, self.pitch_modifier)
             self.stream.write(current_frame)
-            self.stream2.write(current_frame)
+            # self.stream2.write(current_frame)
             #self.stream.write(self.frames[frame_index])
 
             self.stream_in_use = False
--- SoundBoardGUI.py
+++ SoundBoardGUI.py
@@ -1,13 +1,33 @@
 from tkinter import *
+import Sound
+import thread
+import time
 
 window = Tk()
 window.title("Soundboard")
 window.geometry("640x480")
 
-buttonNames = ["dam", "whered you find this", "airhorn"]
+soundCollection = Sound.SoundCollection()
 
-for i in range(0, len(buttonNames)):
-    button = Button(window, text=buttonNames[i])
+def play(sound_entry):
+    print "playing", sound_entry.path_to_sound
+    if not sound_entry.is_playing: # start playing it if it not playing
+        thread.start_new_thread(sound_entry.play, tuple())
+    else: # stop playing it if hold_to_play is off and the key was let go
+        thread.start_new_thread(sound_entry.stop, tuple())
+        counter = 0
+        while sound_entry.stream_in_use and counter < 1000: # wait for the sound_entry to finish outputting its current chunk to the stream if it is in the middle of doing so
+            time.sleep(.001)
+            counter += 1
+        thread.start_new_thread(sound_entry.play, tuple())
+
+
+i = 0
+for k,v in soundCollection.key_bind_map.iteritems():
+    button = Button(window, text=v.path_to_sound, command=v.play)
     button.grid(column=i, row=0)
+    i += 1
 
 window.mainloop()
+
+
